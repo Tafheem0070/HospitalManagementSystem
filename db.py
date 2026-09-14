79c0fcc9a1eb0d2b85ed8add2ed9626319a6cada
Tafheem <tafheema00@gmail.com>
2026-09-14 20:00:16 +0530

    Prepare application for deployment

--- db.py
+++ db.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        port=3306,
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "3306")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
